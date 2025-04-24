4ea27d476cfe0f9ee6a9e2fb815d433cda7b6247
CoffeeJellyMaster <23-03014@g.batstate-u.edu.ph>
2025-04-24 17:31:10 +0800

    init

---
+++ Digital-Diary/Diary.cs
@@ -0,0 +1,21 @@
+﻿namespace Digital_Diary;
+
+public class Diary
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; }
+
+    public Diary(string title, string content)
+    {
+        Title = title;
+        Content = content;
+        CreatedAt = DateTime.Now;
+    }
+
+    public void DisplayEntry()
+    {
+        Console.WriteLine($"\n{Title} - {CreatedAt}");
+        Console.WriteLine(Content);
+    }
+}
---
+++ Digital-Diary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Digital_Diary;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Digital Diary!");
+        
+        var program = new Program(); // Create an instance
+        program.Menu();
+        
+        Diary diary = new Diary("My First Entry", "This is my first entry using .NET 9 and Rider!");
+        diary.DisplayEntry();
+    }
+
+    public void Menu()
+    {
+        Console.WriteLine("Testing Lang\n");
+    }
+    
+    // public static void Menu()
+    // {
+    //     Console.WriteLine("🛠️ Menu function is running...");
+    // }
+    
+}
